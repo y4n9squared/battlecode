2eef04f3cbf72371a828461c57b2bef9add95ec4
Yang Yang <y4n9squared@gmail.com>
2014-12-24 12:05:46 -0800

    Prevent CI environment from downloading checkstyle

--- install.sh
+++ install.sh
@@ -10,9 +10,3 @@ if [ ! -e ~/.ant/lib/ivy.jar ]; then
   wget http://repo2.maven.org/maven2/org/apache/ivy/ivy/2.3.0/ivy-2.3.0.jar
   mv ivy-2.3.0.jar ~/.ant/lib/ivy.jar
 fi
-
-if [ ! -e ~/.ant/lib/checkstyle.jar ]; then
-  echo 'Installing checkstyle...'
-  wget http://downloads.sourceforge.net/projects/checkstyle/checkstyle/6.1.1/checkstyle-6.1.1-all.jar
-  mv checkstyle-6.1.1-all.jar ~/.ant/lib/checkstyle.jar
-fi
