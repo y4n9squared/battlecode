3d3db557a83116622d3340fa2b8feb706b51642e
Yang Yang <y4n9squared@gmail.com>
2015-01-11 12:37:48 -0800

    Implement HQ attack priority

--- src/main/java/zasshu/HQ.java
+++ src/main/java/zasshu/HQ.java
@@ -20,13 +20,45 @@ public final class HQ extends AbstractRobot {
    */
   private static int NUM_BEAVER_TARGET = 1;
 
+  /**
+   * This is the order in which the HQ attacks enemys.
+   */
+  private enum AttackPriority {
+    MISSILE, LAUNCHER, COMMANDER, TANK, DRONE, BASHER, SOLDIER, MINER, BEAVER,
+    COMPUTER;
+  }
+
+  /**
+   * Constructs the HQ.
+   *
+   * @param controller controller object
+   */
   public HQ(Controller controller) {
     super(controller);
   }
 
+
   @Override protected void runHelper() {
     if (controller.isWeaponReady()) {
-      // TODO: Attack intelligently
+      RobotInfo[] enemies = controller.getNearbyRobots(
+          RobotType.HQ.attackRadiusSquared, controller.getOpponentTeam());
+
+      RobotInfo target = null;
+      int maxPriority = AttackPriority.COMPUTER.ordinal();
+
+      for (int i = enemies.length; --i >= 0;) {
+        // Calling Enum.valueOf is potentially dangerous here - if the enemy
+        // RobotType.toString conversion does not match an AttackPriority enum,
+        // the method will throw IllegalArgumentException.
+        int p = AttackPriority.valueOf(enemies[i].type.toString()).ordinal();
+        if (target == null || p < maxPriority
+            || (p == maxPriority && enemies[i].health > target.health)) {
+          target = enemies[i];
+        }
+      }
+      if (target != null) {
+        controller.attack(target);
+      }
     }
 
     if (controller.isCoreReady()) {
