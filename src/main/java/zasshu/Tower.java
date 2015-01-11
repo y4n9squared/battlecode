4a7ccff9d601074b70d4515447837dae56dfc764
Yang Yang <y4n9squared@gmail.com>
2015-01-11 13:44:00 -0800

    Implement tower attack

--- src/main/java/zasshu/Tower.java
+++ src/main/java/zasshu/Tower.java
@@ -8,13 +8,46 @@ package zasshu;
 import zasshu.core.AbstractRobot;
 import zasshu.core.Controller;
 
+import battlecode.common.RobotInfo;
+import battlecode.common.RobotType;
+
 public final class Tower extends AbstractRobot {
+
+  /**
+   * This is the order in which the tower attacks enemys.
+   */
+  private enum AttackPriority {
+    MISSILE, LAUNCHER, COMMANDER, TANK, DRONE, BASHER, SOLDIER, MINER, BEAVER,
+    COMPUTER;
+  }
 
   public Tower(Controller controller) {
     super(controller);
   }
 
   @Override protected void runHelper() {
-    // TODO: Attack intelligently
+    if (controller.isWeaponReady()) {
+      RobotInfo[] enemies = controller.getNearbyRobots(
+          RobotType.TOWER.attackRadiusSquared, controller.getOpponentTeam());
+
+      RobotInfo target = null;
+      int maxPriority = AttackPriority.COMPUTER.ordinal();
+
+      for (int i = enemies.length; --i >= 0;) {
+        // Calling Enum.valueOf is potentially dangerous here - if the enemy
+        // RobotType.toString conversion does not match an AttackPriority enum,
+        // the method will throw IllegalArgumentException.
+        int p = AttackPriority.valueOf(enemies[i].type.toString()).ordinal();
+        if (target == null || p < maxPriority
+            || (p == maxPriority && enemies[i].health > target.health)) {
+          target = enemies[i];
+        }
+      }
+      if (target != null) {
+        controller.attack(target);
+      }
+    }
+    // TODO: Implement a way for the tower to call for help, possibly based on
+    // checking an influence threshold and broadcasting.
   }
 }
