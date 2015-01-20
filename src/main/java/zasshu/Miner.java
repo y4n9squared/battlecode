80605d28f96f728715b9c61b8b91210fb9c1da1f
Yang Yang <y4n9squared@gmail.com>
2015-01-19 18:45:25 -0800

    Improve mining AI

--- src/main/java/zasshu/Miner.java
+++ src/main/java/zasshu/Miner.java
@@ -5,22 +5,27 @@
 
 package zasshu;
 
+import static zasshu.util.Algorithms.*;
+
 import zasshu.core.AbstractRobot;
 import zasshu.core.Controller;
 
-import battlecode.common.DependencyProgress;
 import battlecode.common.Direction;
 import battlecode.common.MapLocation;
+import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
 
+import java.util.Random;
+
+/**
+ * A Miner robot.
+ *
+ * @author Holman Gao
+ * @author Yang Yang
+ */
 public final class Miner extends AbstractRobot {
 
-  /**
-   * A square is considered to have low ore if its ore is below this number.
-   */
-  private static double LOW_ORE = 1.0;
-
-  private int mineCounter = 0;
+  private final Random rnd = new Random();
 
   /**
    * Constructs a {@code Miner} object.
@@ -29,40 +34,103 @@ public final class Miner extends AbstractRobot {
    */
   public Miner(Controller c) {
     super(c);
+    rnd.setSeed(controller.getID());
   }
 
   @Override protected void runHelper() {
     if (controller.isCoreReady()) {
-      boolean moved = false;
-      MapLocation myLoc = controller.getLocation();
+      MapLocation myLoc = getLocation();
+      RobotInfo[] enemies = controller.getNearbyRobots(
+          RobotType.MINER.sensorRadiusSquared, controller.getOpponentTeam());
+      MapLocation[] locs = MapLocation.getAllMapLocationsWithinRadiusSq(
+          myLoc, 2);
+      if (enemies.length > 0) {
+        flee(enemies);
+      } else {
+        double ore = controller.senseOre(myLoc);
+        if (ore >= 10) {
+          controller.mine();
+        } else {
+          move();
+        }
+      }
+    }
+  }
 
-      // For now, let's have a mine:move ratio of 3:1
-      if (mineCounter == 3 || controller.senseOre(myLoc) < LOW_ORE) {
-        double maxOre = 0.0;
-        Direction maxDir = Direction.NONE;
-        MapLocation[] locs =
-            MapLocation.getAllMapLocationsWithinRadiusSq(myLoc, 2);
+  private void flee(RobotInfo[] enemies) {
+    MapLocation myLoc = getLocation();
+    MapLocation[] locs = MapLocation.getAllMapLocationsWithinRadiusSq(
+        myLoc, 2);
+    MapLocation target = null;
+    double maxPotential = Double.NEGATIVE_INFINITY;
+    for (int i = locs.length; --i >= 0;) {
+      if (controller.canMove(myLoc.directionTo(locs[i]))) {
+        double potential = 0;
+        for (int j = enemies.length; --j >= 0;) {
+          int d = locs[i].distanceSquaredTo(enemies[j].location);
+          potential -= 5.0 / d;
+        }
+        if (potential > maxPotential) {
+          maxPotential = potential;
+          target = locs[i];
+        }
+      }
+    }
+    controller.move(myLoc.directionTo(target));
+  }
 
-        for (int i = 8; --i >= 0;) {
-          Direction dir = myLoc.directionTo(locs[i]);
-          if (!controller.canMove(dir)) {
-            continue;
-          }
+  private void move() {
+    MapLocation myLoc = getLocation();
+    MapLocation[] locs = MapLocation.getAllMapLocationsWithinRadiusSq(
+        myLoc, 2);
+    MapLocation[] enemyTowers = controller.getEnemyTowerLocations();
 
-          double ore = controller.senseOre(locs[i]);
-          if (ore > maxOre) {
-            maxOre = ore;
-            maxDir = dir;
+    MapLocation target = null;
+    double maxOre = controller.senseOre(myLoc);
+    for (int i = locs.length; --i >= 0;) {
+      if (!controller.isLocationOccupied(locs[i])) {
+        boolean inTowerRange = false;
+        for (int j = enemyTowers.length; --j >= 0;) {
+          if (locs[i].distanceSquaredTo(enemyTowers[j])
+              <= RobotType.TOWER.attackRadiusSquared) {
+            inTowerRange = true;
+            break;
           }
         }
-        moved = controller.move(maxDir);
+        if (inTowerRange) {
+          continue;
+        }
+        double ore = controller.senseOre(locs[i]);
+        if (ore > maxOre) {
+          maxOre = ore;
+          target = locs[i];
+        }
       }
-
-      if (moved) {
-        mineCounter = 0;
-      } else {
-        controller.mine();
-        ++mineCounter;
+    }
+    if (target != null) {
+      controller.move(myLoc.directionTo(target));
+    } else if (maxOre > 0) {
+      controller.mine();
+    } else {
+      Direction[] dirs = Direction.values();
+      boolean loop = true;
+      while (loop) {
+        int i = rnd.nextInt(8);
+        if (controller.canMove(dirs[i])) {
+          boolean inTowerRange = false;
+          for (int j = enemyTowers.length; --j >= 0;) {
+            if (locs[i].distanceSquaredTo(enemyTowers[j])
+                <= RobotType.TOWER.attackRadiusSquared) {
+              inTowerRange = true;
+              break;
+            }
+          }
+          if (inTowerRange) {
+            continue;
+          }
+          controller.move(dirs[i]);
+          loop = false;
+        }
       }
     }
   }
