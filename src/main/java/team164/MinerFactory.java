1074543e78f89db5f1cc7af8dcb8e486ddfe1d6c
Yang Yang <y4n9squared@gmail.com>
2015-01-22 16:04:37 -0800

    Produce miners based on size of map

--- src/main/java/team164/MinerFactory.java
+++ src/main/java/team164/MinerFactory.java
@@ -14,15 +14,33 @@ import battlecode.common.GameConstants;
 import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
 
+/**
+ * A MinerFactory robot.
+ *
+ * <p>The number of miners produced is based on a logistic function where the
+ * variable is the distance between HQs.
+ */
 public final class MinerFactory extends AbstractRobot {
+
+  private static final int MAX_MINERS = 30;
+  private static final double GROWTH_RATE = 0.01;
 
   /**
    * The number of miners to maintain on the map.
    */
-  private static int NUM_MINER_TARGET = 8;
+  private final int numTargetMiners;
 
+  /**
+   * Constructs a Miner robot.
+   *
+   * @param controller controller
+   */
   public MinerFactory(Controller controller) {
     super(controller);
+    int d = controller.getHQLocation().distanceSquaredTo(
+        controller.getEnemyHQLocation());
+    numTargetMiners = (int) (MAX_MINERS / (1 + Math.pow(
+            Math.E, -1 * GROWTH_RATE * Math.sqrt(d))));
   }
 
   @Override protected void runHelper() {
@@ -38,7 +56,7 @@ public final class MinerFactory extends AbstractRobot {
         }
       }
 
-      if (numMiners < NUM_MINER_TARGET) {
+      if (numMiners < numTargetMiners) {
         controller.spawn(getEnemyHQDirection(), RobotType.MINER);
       }
     }
